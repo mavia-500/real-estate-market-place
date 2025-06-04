d70b5aec501131611ef22e3306b889a899fa2448
ITfiers-Dev <itfiers@gmail.com>
2025-06-04 20:12:32 +0500

    changes in backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from "./routes/auth.routes.js"
 dotenv.config({path:'../.env'});
 
-
+const app =express();
 mongoose
 .connect(process.env.MONGO)
 .then(()=>{
@@ -14,7 +14,9 @@ mongoose
     console.log("eroor",error)
 })
 
-const app =express();
+
+app.use(express.json());
+
 const port=3000
 
 app.listen(port,()=>{
@@ -25,3 +27,13 @@ app.listen(port,()=>{
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+
+app.use((err,req,res,next)=>{
+const statusCode=err.statusCode || 500
+const message=err.message || 'internal server error'
+return res.status(statusCode).json({
+    success:false,
+    statusCode:statusCode,
+message,
+})
+})
