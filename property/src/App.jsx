d47c291348dac839363cb91e2a86829e5a5f8be2
ITfiers-Dev <itfiers@gmail.com>
2025-06-04 16:02:55 +0500

    bassic structure

--- property/src/App.jsx
+++ property/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import About from './pages/about'
+import Profile from './pages/Profile'
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      
+      <Route path='/sign-up' element={<SignOut/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+    
+    </>
   )
 }
 
